c3013c419d7116f60bbf41ee514333120da10ea7
fabiolucassc <fabio-lucas1.000@hotmail.com>
2021-07-09 17:37:15 -0300

    Create setOf

--- src/main/kotlin/one/digitalinnovation/collection/Funcionario.kt
+++ src/main/kotlin/one/digitalinnovation/collection/Funcionario.kt
@@ -1,4 +1,4 @@
-package one.digitalinnovation.collections
+package one.digitalinnovation.collection
 
 data class Funcionario(
     val nome: String,
--- src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
@@ -1,4 +1,4 @@
-package one.digitalinnovation.collections
+package one.digitalinnovation.collection
 
 fun main() {
     val joao = Funcionario("Joao", 2000.0, "CLT")
